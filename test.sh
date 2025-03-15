4af18d07141b3cf270dd821777b35e395bb1af46
MasterZeeno <zeenoliev@gmail.com>
2025-03-16 06:44:36 +0800

    Init

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "$ORIG_AUTHOR"
-echo "$ORIG_REPO_NAME"
-echo "$ORIG_REPO_YT_ID"
-echo "$ORIG_PATH_NAME"
-echo "$MOD_AUTHOR"
-echo "$MOD_REPOSITORY"
+MOD_REPO_NAME=$(basename "$MOD_REPOSITORY")
+MOD_REPO_YT_ID="$(echo "${MOD_REPO_NAME,,}" | cut -d'-' -f1)-yt"
+MOD_PATH_NAME="$(echo "${MOD_REPO_NAME^^}" | cut -d'-' -f1)PATH"
+
+echo "MOD_AUTHOR: $MOD_AUTHOR"
+echo "MOD_REPOSITORY: $MOD_REPOSITORY"
+echo "MOD_REPO_NAME: $MOD_REPO_NAME"
+echo "MOD_REPO_YT_ID: $MOD_REPO_YT_ID"
+echo "MOD_PATH_NAME: $MOD_PATH_NAME"
