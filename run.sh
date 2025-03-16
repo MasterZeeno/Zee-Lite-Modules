900aa3ac4a9ec0cd0e0d8a552d115c7542811e53
MasterZeeno <zeenoliev@gmail.com>
2025-03-16 08:36:59 +0800

    Init

--- run.sh
+++ run.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
+[[ -z "$MOD_REPO_NAME" ]] && exit 1
 CUR_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
-MOD_REPO_NAME=$(basename "$MOD_REPOSITORY")
-MOD_REPO_YT_ID="$(echo "${MOD_REPO_NAME,,}" | cut -d'-' -f1)-yt"
-MOD_PATH_NAME="$(echo "${MOD_REPO_NAME^^}" | cut -d'-' -f1)PATH"
-MOD_NAME_YT="$MOD_AUTHOR YouTube Lite"
-MOD_DESC_YT="$MOD_NAME_YT Magisk Module"
-SITE_URL='https://github.com'
-SITE_API='https://api.github.com'
-SITE_RAW='https://raw.githubusercontent.com'
-ZIP_URL="$SITE_URL/$MOD_AUTHOR/$MOD_REPO_NAME/releases/download"
-LATEST_URL="$SITE_API/repos/$ORIG_AUTHOR/$ORIG_REPO_NAME/releases/latest"
-UPDATE_URL_RAW="$SITE_RAW/$MOD_AUTHOR/$MOD_REPO_NAME/main/$MOD_REPO_YT_ID"
-TO_MATCH='VERSION='
-CUSTOM_FX='RESOLVE_VERSION'
-TO_APPEND="VERSION=\$($CUSTOM_FX \"\$VERSION\")"
-TO_DELETE='Join t.me'
 [[ ${1:-0} -eq 1 ]] && : > "$CUR_DIR/TAG"
 mkdir_safe() {
   local dir="$1"
